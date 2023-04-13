61d000ec2db6a3a468ed23bdf58bf621071a88c8
Rafael Strack <strackinado@gmail.com>
2023-04-13 01:33:51 -0300

    implementação do teste de uniformidade, organização de código

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
 
+import tests
 from gnpcl import Generator
 
-EXP = 13
+EXP = 17
 MOD = float(2**EXP - 1)
 #a = MOD**(0.5)
 a = float(2**(EXP/2))
-c = 773
+c = 7
 
+#valores aleatorios iniciais
 SEEDS = [
     123,
     126540,
@@ -21,12 +23,21 @@ SEEDS = [
     465631163
 ]
 
-for file_no in range(1, 11):
-    with open(f'./NEWALEO_{file_no}.txt', 'w') as file:
-        start_time = time.time()
-        gen = Generator(SEEDS[file_no-1], EXP, MOD, c)
-        for i in range(5000000):
-            x = gen.generate()
-            file.write(f'{x}\n')
-        duration = time.time() - start_time
-        print(f'Arquivo {file.name} criado em {duration:.2f} segundos')
+def generate_files():
+    for file_no in range(1, 11):
+        with open(f'./NEWALEO_{file_no}.txt', 'w') as file:
+            start_time = time.time()
+            gen = Generator(SEEDS[file_no-1], EXP, MOD, c)
+            for i in range(5000000):
+                x = gen.generate()
+                file.write(f'{x}\n')
+            duration = time.time() - start_time
+            print(f'Arquivo {file.name} criado em {duration:.2f} segundos')
+
+def execute_tests():
+    for file_no in range(10):
+        tests.uniformity_test(f'./NEWALEO_{file_no+1}.txt')
+
+if __name__ == '__main__':
+    #generate_files()
+    execute_tests()
