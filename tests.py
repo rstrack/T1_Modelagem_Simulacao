46ddd606e548c60ae87df4e21b77233dfaf0b558
Rafael Strack <strackinado@gmail.com>
2023-04-16 23:57:05 -0300

    teste de permutação: adição de tamanho dinâmico de grupos de permutação

--- tests.py
+++ tests.py
@@ -1,12 +1,16 @@
+import itertools
 import numpy as np
 import math
 
-CLASS_COUNT = 10
+UNIFORMITY_CLASS_COUNT = 10
 NUM_COUNT = 5000000
+PERMUTATION_ELEMENT_COUNT = 5
+PERMUTATION_COMB_COUNT = math.factorial(PERMUTATION_ELEMENT_COUNT)
+
 
 def uniformity_test(file_path: str):
     #array que armazena a frequencia de cada classe
-    values = np.zeros(CLASS_COUNT, dtype=float)
+    values = np.zeros(UNIFORMITY_CLASS_COUNT, dtype=float)
 
     with open(file_path, 'r') as f:
         cont = 0
@@ -14,14 +18,14 @@ def uniformity_test(file_path: str):
             if cont == NUM_COUNT: break
             cont += 1
             value = float(line)
-            values[int(value*CLASS_COUNT)] += 1
+            values[int(value*UNIFORMITY_CLASS_COUNT)] += 1
 
     #frequencia acumulada esperada
-    expected_cdf = np.array([1/CLASS_COUNT*i for i in range(1, CLASS_COUNT+1)]) 
+    expected_cdf = np.array([1/UNIFORMITY_CLASS_COUNT*i for i in range(1, UNIFORMITY_CLASS_COUNT+1)]) 
 
     #frequencia acumulada observada
-    observed_cdf = np.zeros(CLASS_COUNT, dtype=float) 
-    for i in range(CLASS_COUNT):
+    observed_cdf = np.zeros(UNIFORMITY_CLASS_COUNT, dtype=float) 
+    for i in range(UNIFORMITY_CLASS_COUNT):
         observed_cdf[i] = sum(values[0:i+1])/NUM_COUNT
 
     ks_calc = np.max(np.abs(observed_cdf - expected_cdf))
@@ -37,6 +41,8 @@ def uniformity_test(file_path: str):
 
 
 def runs_test(file_path: str):
+    
+    results = [False] * 2
     asc_run_value = 1
     desc_run_value = 1
     asc_runs = list()
@@ -82,15 +88,18 @@ def runs_test(file_path: str):
     desc_ks_5 = 1.36/(np.sqrt(len(desc_runs)))
     print(f'CORRIDA ASCENDENTE: KS Calculado: {asc_ks_calc}\nKS 5%: {asc_ks_5}')
     print(f'CORRIDA DESCENDENTE: KS Calculado: {desc_ks_calc}\nKS 5%: {desc_ks_5}')
-
-    if(asc_ks_calc < asc_ks_5 and desc_ks_calc < desc_ks_5):
-        print('Aceita H0')
-        return True
-    else: 
-        print('Rejeita H0')
-        if asc_ks_calc > asc_ks_5: print('Falha na corrida ascendente')
-        if desc_ks_calc > desc_ks_5: print('Falha na corrida descendente')
-        return False
+    
+    if asc_ks_calc < asc_ks_5:
+        results[0] = True
+        print('CORRIDA ASCENDENTE OK')
+    else:
+        print('Falha na corrida ascendente')
+    if desc_ks_calc < desc_ks_5:
+        results[0] = True
+        print('CORRIDA DESCENDENTE OK')
+    else:
+        print('Falha na corrida descendente')
+    return results
     
 
 def interval_test(file_path: str):
@@ -110,11 +119,11 @@ def interval_test(file_path: str):
             else:
                 intervals.append(interval_len)
                 interval_len = 0
-        class_count = np.max(intervals)+1
-        expected = np.array([(0.9**k)*0.1 for k in range(class_count)])
-        expected_cdf = np.array([(expected[k] if k == 0 else sum(expected[:k+1])) for k in range(class_count)])
-        observed_cdf = np.zeros(class_count)
-        for i in range(class_count):
+        UNIFORMITY_CLASS_COUNT = np.max(intervals)+1
+        expected = np.array([(0.9**k)*0.1 for k in range(UNIFORMITY_CLASS_COUNT)])
+        expected_cdf = np.array([(expected[k] if k == 0 else sum(expected[:k+1])) for k in range(UNIFORMITY_CLASS_COUNT)])
+        observed_cdf = np.zeros(UNIFORMITY_CLASS_COUNT)
+        for i in range(UNIFORMITY_CLASS_COUNT):
             observed_cdf[i] = intervals.count(i)/len(intervals) if i == 0 else intervals.count(i)/len(intervals) + observed_cdf[i-1]
         
         ks_calc = np.max(np.abs(observed_cdf - expected_cdf))
@@ -129,18 +138,19 @@ def interval_test(file_path: str):
 
 
 def permutation_test(file_path: str):
-    fo_count = [0] * 6
+    fo_count = [0] * 120
     with open(file_path, 'r') as f:
         values = [float(line) for line in f.readlines()]
 
-    values = values[:-(len(values)%3)]
+    # values = values[:-(len(values)%3)]
     
     for i in range(len(values)):
-        if ((i+1) % 3) == 0:
-            fo_count[order_func(values[i-2], values[i-1], values[i])] += 1
+        if ((i+1) % PERMUTATION_ELEMENT_COUNT) == 0:
+            fo_count[order_func(values[i-4:i+1], PERMUTATION_ELEMENT_COUNT)] += 1
+    
     group_count = sum(fo_count)
-    expected_cdf = np.array([(1/6)*i for i in range(1,7)])
-    observed_cdf = np.array([sum(fo_count[:i+1])/group_count for i in range(6)])
+    expected_cdf = np.array([(1/PERMUTATION_COMB_COUNT)*i for i in range(1,PERMUTATION_COMB_COUNT+1)])
+    observed_cdf = np.array([sum(fo_count[:i+1])/group_count for i in range(PERMUTATION_COMB_COUNT)])
     ks_calc = np.max(np.abs(observed_cdf - expected_cdf))
     ks_5 = 1.36/np.sqrt(group_count)
     print(f'TESTE DE PERMUTAÇÃO: KS Calculado: {ks_calc} | KS 5%: {ks_5}')
@@ -152,17 +162,10 @@ def permutation_test(file_path: str):
         return False
 
 
-def order_func(v1:float, v2:float, v3:float):
-    if v1 < v2 and v2 < v3:
-        return 0
-    elif v1 < v3 and v3 < v2:
-        return 1
-    elif v2 < v1 and v1 < v3:
-        return 2
-    elif v2 < v3 and v3 < v1:
-        return 3
-    elif v3 < v1 and v1 < v2:
-        return 4
-    elif v3 < v2 and v2 < v1:
-        return 5
-    else: return 'error'
+def order_func(values: list, comb_length: int):
+    combs = list(itertools.permutations(sorted(values), 5))
+    if comb_length < 3:
+        raise ValueError("O tamanho das combinações deve ser maior ou igual a 3")
+    for i, comb in enumerate(combs):
+        if comb == tuple(values):
+            return i
